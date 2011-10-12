24a1adf4b87273d60d134866e8e17a8fd8335853
Yoon Young Min <airyym@gmail.com>
2011-10-12 10:19:25 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCms::Application.config.secret_token = '5b774142753382cb3f2d77bf0bebb2f2e63bc29b2f57d04a08c709baba9fe153be7a3f7a501c9aad5ff5c261d018e6ba8e5127d376d23c450c24db6329417fb1'
