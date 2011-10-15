fc69fb7d83cf4fc0019334a55bf4d3c623256458
myoon <ymyoon84@rare2wear.com>
2011-10-15 13:13:02 +0900

    ch 8 end

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,7 +1,10 @@
 class AdminUser < ActiveRecord::Base
   
   #set_table_name("admin_users")
-
- scope :named, lambda {|first, last| where(:first_name => first, :last_name => last)}  
+  has_and_belongs_to_many :pages
+  has_many :section_edits
+  has_many :sections, :through => :section_edits
+  
+  scope :named, lambda {|first, last| where(:first_name => first, :last_name => last)}  
   
 end
