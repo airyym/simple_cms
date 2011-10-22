0da6779bb8e6aaee1d7ac2c5ef01e19350066692
myoon <ymyoon84@rare2wear.com>
2011-10-22 22:07:29 +0900

    ...

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -13,4 +13,17 @@ class SubjectsController < ApplicationController
     @subject = Subject.find_by_id(params[:id])
   end
   
+  def new
+    @subject = Subject.new(:name => "default")
+  end
+  
+  def create
+    @subject = Subject.new(params[:subject])
+    if @subject.save
+      redirect_to(:action => 'list')
+    else
+      render('new')
+    end
+  end
+  
 end
